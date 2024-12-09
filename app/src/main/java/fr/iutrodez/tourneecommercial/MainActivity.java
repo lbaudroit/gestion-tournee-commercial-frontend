cb38392f9bc1d8cf8d521a6fdc9c297653f5f849
Chaipo <ahmed.bribach@iut-rodez.fr>
2024-12-09 15:14:51 +0100

    debut de l'interface de création du client

--- app/src/main/java/fr/iutrodez/tourneecommercial/MainActivity.java
+++ app/src/main/java/fr/iutrodez/tourneecommercial/MainActivity.java
@@ -1,8 +1,15 @@
 package fr.iutrodez.tourneecommercial;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_clients);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Création nouveau client");
+        }
+
+
+
     }
 }
