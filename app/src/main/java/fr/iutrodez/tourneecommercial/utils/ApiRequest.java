1feab8bbe089f1f3fafdbb520718c1fba6639f6a
Benicol <133012549+Benicol@users.noreply.github.com>
2025-01-24 10:07:00 +0100

    Fix : ApiRequest is corrected to use genericity

--- app/src/main/java/fr/iutrodez/tourneecommercial/utils/ApiRequest.java
+++ app/src/main/java/fr/iutrodez/tourneecommercial/utils/ApiRequest.java
@@ -1,27 +1,43 @@
 package fr.iutrodez.tourneecommercial.utils;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Context;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import fr.iutrodez.tourneecommercial.R;
 
 public class ApiRequest {
     private static RequestQueue requestQueue;
     private static final String API_URL = "http://10.0.2.2:9090/";
 
+<<<<<<< HEAD
 
     public interface ApiResponseCallback {
         void onSuccess(JSONObject response);
+=======
+    public interface ApiResponseCallback<T> {
+        void onSuccess(T response);
+>>>>>>> e5cc8f9 (Fix : ApiRequest is corrected to use genericity)
 
         void onError(VolleyError error);
     }
 
-    public static void connexion(Context context, String url, JSONObject postData, ApiResponseCallback callback) {
+    public static void connexion(Context context, String url, JSONObject postData, ApiResponseCallback<JSONObject> callback) {
         if (requestQueue == null) {
             requestQueue = Volley.newRequestQueue(context);
         }
@@ -37,7 +53,7 @@ public class ApiRequest {
         requestQueue.add(jsonObjectRequest);
     }
 
-    public static void inscription(Context context, String url, JSONObject postData, ApiResponseCallback callback) {
+    public static void inscription(Context context, String url, JSONObject postData, ApiResponseCallback<JSONObject> callback) {
         if (requestQueue == null) {
             requestQueue = Volley.newRequestQueue(context);
         }
@@ -53,7 +69,7 @@ public class ApiRequest {
         requestQueue.add(jsonObjectRequest);
     }
 
-    public static void creationClient(Context context, String url, JSONObject postData, ApiResponseCallback callback) {
+    public static void creationClient(Context context, String url, JSONObject postData, ApiResponseCallback<JSONObject> callback) {
         if (requestQueue == null) {
             requestQueue = Volley.newRequestQueue(context);
         }
@@ -68,7 +84,7 @@ public class ApiRequest {
         requestQueue.add(jsonObjectRequest);
     }
 
-    public static void validationAdresse(Context context, String libelleAdresse, String codePostal, String ville, ApiResponseCallback callback) {
+    public static void validationAdresse(Context context, String libelleAdresse, String codePostal, String ville, ApiResponseCallback<JSONObject> callback) {
         if (requestQueue == null) {
             requestQueue = Volley.newRequestQueue(context);
         }
@@ -84,7 +100,7 @@ public class ApiRequest {
         requestQueue.add(jsonObjectRequest);
     }
 
-    public static void fetchAddressSuggestions(Context context, String query, ApiResponseCallback callback) {
+    public static void fetchAddressSuggestions(Context context, String query, ApiResponseCallback<JSONObject> callback) {
         if (requestQueue == null) {
             requestQueue = Volley.newRequestQueue(context);
         }
@@ -97,6 +113,112 @@ public class ApiRequest {
                 callback::onError
         );
         requestQueue.add(jsonObjectRequest);
+    }
+
+    public static void fetchNombresItineraires(Context context, String url, ApiResponseCallback<JSONObject> callback) {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context);
+        }
+        String token = getAPI_KEY(context);
+        JsonObjectRequest jsonArrayRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                API_URL + url,
+                null,
+                callback::onSuccess,
+                callback::onError
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    public static void fetchItineraires(Context context, String url, ApiResponseCallback<JSONArray> callback) {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context);
+        }
+        String token = getAPI_KEY(context);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                API_URL + url,
+                null,
+                callback::onSuccess,
+                callback::onError
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+        requestQueue.add(jsonArrayRequest);
+    }
+
+    public static void deleteItineraire(Context context, long itineraireId, ApiResponseCallback<JSONObject> callback) {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context);
+        }
+        String token = getAPI_KEY(context);
+        System.out.println(API_URL + "itineraire/supprimer/?id=" + itineraireId);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.DELETE,
+                API_URL + "itineraire/supprimer/?id=" + itineraireId,
+                null,
+                callback::onSuccess,
+                callback::onError
+        ) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+        requestQueue.add(jsonObjectRequest);
+
+    }
+
+    public static String getAPI_KEY(Context context) {
+        final String[] token = {context.getSharedPreferences("user", MODE_PRIVATE).getString("token", "")};
+        long expirationTime = context.getSharedPreferences("user", MODE_PRIVATE).getLong("expiration", 0);
+        if (expirationTime < System.currentTimeMillis()) {
+            JSONObject postData = new JSONObject();
+            try {
+                postData.put("email", context.getSharedPreferences("user", MODE_PRIVATE).getString("email", ""));
+                postData.put("motDePasse", context.getSharedPreferences("user", MODE_PRIVATE).getString("password", ""));
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+            ApiRequest.connexion(context, "auth/authentifier", postData, new ApiRequest.ApiResponseCallback<JSONObject>() {
+                @Override
+                public void onSuccess(JSONObject response) {
+                    // Récupération du token
+                    try {
+                        token[0] = response.getString("token");
+                        // Current time in milliseconde
+                        long expirationTime = System.currentTimeMillis() + response.getLong("expiration");
+                        // Enregistrement du token dans les SharedPreferences
+                        context.getSharedPreferences("user", MODE_PRIVATE).edit().putLong("expiration", expirationTime).apply();
+                        context.getSharedPreferences("user", MODE_PRIVATE).edit().putString("token", token[0]).apply();
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onError(VolleyError error) {
+                    Toast.makeText(context, R.string.invalid_params_connexion_error, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+        System.out.println("after : " + context.getSharedPreferences("user", MODE_PRIVATE).getLong("expiration", 0));
+        return token[0];
     }
 
 }
